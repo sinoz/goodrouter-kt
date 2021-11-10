ef45c89d9e1b9d8d7f61dc116ee4fff3e917764f
sino <ilyas.alfan.b@gmail.com>
2021-11-10 11:27:30 +0100

    Add public modifier

--- src/test/kotlin/com/github/sinoz/goodrouter/LibrarySpec.kt
+++ src/test/kotlin/com/github/sinoz/goodrouter/LibrarySpec.kt
@@ -6,7 +6,7 @@ package com.github.sinoz.goodrouter
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
-class LibrarySpec : StringSpec({
+public class LibrarySpec : StringSpec({
     "someLibraryMethod should return 'true'" {
         val classUnderTest = Library()
         classUnderTest.someLibraryMethod() shouldBe true
