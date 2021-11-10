b9b1bb91c19d65a4d79893e38d58a425d35a39a5
sino <ilyas.alfan.b@gmail.com>
2021-11-10 11:22:12 +0100

    Update gradle project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,36 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.5.0"
+    kotlin("jvm")
 }
 
-repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
-}
+allprojects {
+    group = "com.github.sinoz.goodrouter"
+    version = "1.0-SNAPSHOT"
 
-dependencies {
-    // Align versions of all Kotlin components
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    repositories {
+        mavenCentral()
+    }
 
-    // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    tasks {
+        withType<KotlinCompile> {
+            kotlinOptions {
+                jvmTarget = JavaVersion.VERSION_11.toString()
+                freeCompilerArgs = listOf(
+                    "-Xopt-in=kotlin.ExperimentalStdlibApi", "-XXLanguage:+InlineClasses"
+                )
+            }
+        }
 
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:30.1.1-jre")
+        withType<Test> {
+            useJUnitPlatform()
+        }
 
-    // Use the Kotlin test library.
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-    // Use the Kotlin JUnit integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.apache.commons:commons-math3:3.6.1")
+        // Force Gradle to use UTF-8 as character encoding to help Windows
+        // systems with compiling strange characters.
+        withType<JavaCompile> {
+            options.encoding = "UTF-8"
+            options.release.set(11)
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,7 @@
 rootProject.name = "goodrouter-kt"
+
+pluginManagement {
+    plugins {
+        kotlin("jvm") version "1.5.21"
+    }
+}
