20d13bde8200ddb534fcfdaf43ef9eaabbcf01c9
sino <ilyas.alfan.b@gmail.com>
2021-11-10 11:54:15 +0100

    Add URIDecoder, URIEncoder

--- src/main/kotlin/com/github/sinoz/goodrouter/Router.kt
+++ src/main/kotlin/com/github/sinoz/goodrouter/Router.kt
@@ -1,6 +1,12 @@
 package com.github.sinoz.goodrouter
 
-public class Router {
+import com.github.sinoz.goodrouter.uri.URIDecoder
+import com.github.sinoz.goodrouter.uri.URIEncoder
+
+public class Router(
+    public val decoder: URIDecoder = URIDecoder.ECHO,
+    public val encoder: URIEncoder = URIEncoder.ECHO,
+) {
     public fun parse(path: String): Route? {
         return null
     }
