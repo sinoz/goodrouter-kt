fc9c24cc18e262c8f75198243e1a7898b4e4a8fd
sino <ilyas.alfan.b@gmail.com>
2021-11-10 11:47:26 +0100

    Add Router

---
+++ src/main/kotlin/com/github/sinoz/goodrouter/Router.kt
@@ -0,0 +1,14 @@
+package com.github.sinoz.goodrouter
+
+public class Router {
+    public fun parse(path: String): Route? {
+        return null
+    }
+
+    public operator fun set(name: String, template: String) {
+    }
+
+    public fun stringify(route: Route): String? {
+        return null
+    }
+}
