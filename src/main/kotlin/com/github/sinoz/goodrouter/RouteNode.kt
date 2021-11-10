25d6e9ba7bdf13002c1f2a92bc2afc03c0ce1ba9
sino <ilyas.alfan.b@gmail.com>
2021-11-10 11:47:22 +0100

    Make RouteNode comparable, add companion functions

--- src/main/kotlin/com/github/sinoz/goodrouter/RouteNode.kt
+++ src/main/kotlin/com/github/sinoz/goodrouter/RouteNode.kt
@@ -1,9 +1,49 @@
 package com.github.sinoz.goodrouter
 
-public class RouteNode {
+public class RouteNode : Comparable<RouteNode> {
     public var name = ""
     public var anchor = ""
     public var parameter: String? = null
     public val children = mutableListOf<RouteNode>()
     public var parent: RouteNode? = null
+
+    public val root: RouteNode?
+        get() {
+            var node = parent
+            while (node != null) {
+                node = node.parent
+            }
+
+            return node
+        }
+
+    public override fun compareTo(other: RouteNode): Int {
+        return 0
+    }
+
+    public companion object {
+        public fun make(name: String, template: String): RouteNode {
+            return RouteNode()
+        }
+
+        public fun stringify(
+            node: RouteNode?,
+            params: Map<String, String>,
+            encode: (String) -> String
+        ): String {
+            return ""
+        }
+
+        public fun parse(
+            node: RouteNode?,
+            path: String,
+            decode: (String) -> String,
+            params: Map<String, String> = mapOf()
+        ): Route? {
+            return null
+        }
+
+        public fun optimize(newNode: RouteNode) {
+        }
+    }
 }
